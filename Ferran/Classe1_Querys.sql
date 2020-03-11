b34adfcf0acfec7f24094aace3e78b85ffbdfbae
Ferrancf5 <ferrancf96@gmail.com>
2020-03-11 20:59:32 +0100

    Primeres Querys

---
+++ Ferran/Classe1_Querys.sql
@@ -0,0 +1,8 @@
+CREATE table TablaFerran (c1 int(11), c2 int(11));
+
+CREATE TABLE TablaFerran2 (c1 int(11), c2 varchar(64));
+
+DROP TABLE TablaFerran2; 
+
+ALTER TABLE TablaFerran ADD PRIMARY KEY (c1);
+
