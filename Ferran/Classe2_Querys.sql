d111f4435c5100216d16d7cac569cda48b7a69a1
Ferrancf5 <ferrancf96@gmail.com>
2020-03-23 16:32:30 +0100

    exercicis classe

--- Ferran/Classe2_Querys.sql
+++ Ferran/Classe2_Querys.sql
@@ -1,28 +1,102 @@
-SELECT c.customerName, e.firstName , e.jobTitle 
-FROM classicmodels.customers as c
-LEFT JOIN classicmodels.employees  as e 
-ON e.employeeNumber = c.salesRepEmployeeNumber 
-ORDER BY jobTitle 
+SELECT DATE_FORMAT(o.orderDate, '%Y-%m') AS 'Year_Month',
+o.orderNumber
+FROM orders AS o
+WHERE o.status != 'Cancelled' 
+AND DATE_FORMAT(o.orderDate, '%Y-%m') BETWEEN '2003-06' AND '2004-05'
+ORDER BY 'Year_Month'
+-- 10.127 a 10.302
 
-SELECT c.customerName 
-FROM classicmodels.customers as c
-WHERE c.salesRepEmployeeNumber IS NULL 
+SELECT DATE_FORMAT(o.orderDate, '%Y-%m') AS 'Year_Month',
+o.orderNumber
+FROM orders AS o
+WHERE o.status != 'Cancelled' 
+AND DATE_FORMAT(o.orderDate, '%Y-%m') BETWEEN '2004-06' AND '2005-05'
+ORDER BY 'Year_Month'
+-- 10.254 a 10.425
 
-SELECT c.customerName 
-FROM classicmodels.customers as c
-WHERE c.salesRepEmployeeNumber IS NOT NULL 
 
+-- Ejercico A
+-- FY2004
+SELECT products.productName,
+orderdetails.productCode,
+SUM(orderdetails.quantityOrdered) AS "ProducteTotal"
+FROM orderdetails 
+INNER JOIN products ON orderdetails.productCode = products.productCode
+WHERE orderdetails.orderNumber BETWEEN 10127 AND 10302
+GROUP BY orderdetails.productCode
+ORDER BY ProducteTotal DESC 
+LIMIT 0,10
 
+SELECT products.productName,
+orderdetails.productCode,
+-- SUM(orderdetails.quantityOrdered * orderdetails.priceEach) AS "PrecioVentaTotal",
+-- SUM(orderdetails.quantityOrdered * products.buyPrice) AS "PrecioCompraTotal",
+(SUM(orderdetails.quantityOrdered * orderdetails.priceEach) - 
+SUM(orderdetails.quantityOrdered * products.buyPrice)) AS "GananciasTotales"
+FROM orderdetails
+INNER JOIN products ON orderdetails.productCode = products.productCode 
+WHERE orderdetails.orderNumber BETWEEN 10127 AND 10302
+GROUP BY orderdetails.productCode
+ORDER BY GananciasTotales DESC
+LIMIT 0,10
 
-SELECT *
-FROM classicmodels.customers as c
-WHERE c.salesRepEmployeeNumber IS NULL  
+-- FY2005 
+SELECT products.productName,
+orderdetails.productCode,
+SUM(orderdetails.quantityOrdered) AS "ProducteTotal"
+FROM orderdetails
+INNER JOIN products ON orderdetails.productCode = products.productCode
+WHERE orderdetails.orderNumber BETWEEN 10254 AND 10425
+GROUP BY orderdetails.productCode
+ORDER BY ProducteTotal DESC 
+LIMIT 0,10
 
-select c.customerName, count(o.orderNumber) 
-FROM classicmodels.orders AS o 
-RIGHT JOIN classicmodels.customers as c
-ON o.customerNumber = c.customerNumber 
-WHERE c.salesRepEmployeeNumber IS NULL
-GROUP BY c.customerName 
+SELECT products.productName,
+orderdetails.productCode,
+-- SUM(orderdetails.quantityOrdered * orderdetails.priceEach) AS "PrecioVentaTotal",
+-- SUM(orderdetails.quantityOrdered * products.buyPrice) AS "PrecioCompraTotal",
+(SUM(orderdetails.quantityOrdered * orderdetails.priceEach) - 
+SUM(orderdetails.quantityOrdered * products.buyPrice)) AS "GananciasTotales"
+FROM orderdetails
+INNER JOIN products ON orderdetails.productCode = products.productCode 
+WHERE orderdetails.orderNumber BETWEEN 10254 AND 10425
+GROUP BY orderdetails.productCode
+ORDER BY GananciasTotales DESC
+LIMIT 0,10
 
-SELECT
+-- Ejercico B
+-- FY2004
+SELECT products.productLine,
+SUM(orderdetails.quantityOrdered) AS "LiniaTotal"
+FROM products
+RIGHT OUTER JOIN orderdetails ON orderdetails.productCode = products.productCode
+WHERE orderdetails.orderNumber BETWEEN 10127 and 10302
+GROUP BY products.productLine 
+ORDER BY liniaTotal DESC
+
+SELECT products.productLine,
+(SUM(orderdetails.quantityOrdered * orderdetails.priceEach) - 
+SUM(orderdetails.quantityOrdered * products.buyPrice)) AS "GananciasLinia"
+FROM products
+RIGHT OUTER JOIN orderdetails ON orderdetails.productCode = products.productCode
+WHERE orderdetails.orderNumber BETWEEN 10127 and 10302
+GROUP BY products.productLine 
+ORDER BY GananciasLinia DESC
+
+-- FY2005
+SELECT products.productLine,
+SUM(orderdetails.quantityOrdered) AS "LiniaTotal"
+FROM products
+RIGHT OUTER JOIN orderdetails ON orderdetails.productCode = products.productCode
+WHERE orderdetails.orderNumber BETWEEN 10254 AND 10425
+GROUP BY products.productLine 
+ORDER BY liniaTotal DESC
+
+SELECT products.productLine,
+(SUM(orderdetails.quantityOrdered * orderdetails.priceEach) - 
+SUM(orderdetails.quantityOrdered * products.buyPrice)) AS "GananciasLinia"
+FROM products
+RIGHT OUTER JOIN orderdetails ON orderdetails.productCode = products.productCode
+WHERE orderdetails.orderNumber BETWEEN 10254 AND 10425
+GROUP BY products.productLine 
+ORDER BY GananciasLinia DESC
--- Ferran/Classe3_Querys.sql
+++
@@ -1,6 +0,0 @@
-/* Primer ejercicio */
-SELECT COUNT(orders.customerNumber ) AS NumeroDeOrdenes, customerNumber
-FROM orders 
-GROUP BY customerNumber 
-ORDER BY NumeroDeOrdenes DESC 
-/*Segon ejercicio*/
